86996dcff20d16307b5326a63874464fc0d05db6
daramsreya83@gmail.com <your.email@example.com>
2025-07-24 15:05:38 +0530

    sort

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const ul=document.getElementById("band");
+const items = [...ul.querySelectorAll("li")];
+items.sort((a, b) =>
+  a.textContent.replace(/^(a |an |the )/i, "").localeCompare(
+    b.textContent.replace(/^(a |an |the )/i, "")
+  )
+);
+
+ul.innerHTML = "";
+items.forEach(item => ul.appendChild(item));
