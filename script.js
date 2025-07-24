118c0b4f10b214383250845502ff9f97ab4964d5
daramsreya <daramsreya83@gmail.com>
2025-07-24 15:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const ul=document.getElementById("band");
+const ul=document.getElementById("bands");
 const items = [...ul.querySelectorAll("li")];
 items.sort((a, b) =>
   a.textContent.replace(/^(a |an |the )/i, "").localeCompare(
